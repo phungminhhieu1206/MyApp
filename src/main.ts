5bfe7edbe34394faa5cbcae0bc7b1cc8855afec4
Phung Minh Hieu <hieuphung1206@Phungs-MacBook-Pro.local>
2022-09-15 19:19:46 +0700

    init project to study angular

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/**
+ * Đầu tiên sẽ kiểm tra mỗi trường đang sử dụng là gì?
+ * environment lấy từ thư mục src.environments, hiện tại chỉ có môi trường production
+ */
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/**
+ * Angular is a modular framework
+ * Nghĩa là: Tất cả mọi thứ trong angular đều là module
+ */
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
