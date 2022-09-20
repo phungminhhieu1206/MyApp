206823f8a59eb28b30e18d600715327304286f12
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2022-09-20 23:57:52 +0700

    done guard for my cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { CartComponent } from './pages/cart/cart.component';
 import { BooksComponent } from './pages/books/books.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: BooksComponent },
-  { path: 'cart', component: CartComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
